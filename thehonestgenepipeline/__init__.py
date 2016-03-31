3eb84f2904414628cb46169a349b33e706916baa
Uemit Seren <uemit.seren@gmail.com>
2016-03-31 17:23:02 +0200

    BASE_FOLDER is not mandatory anymore

--- thehonestgenepipeline/__init__.py
+++ thehonestgenepipeline/__init__.py
@@ -1,5 +1,5 @@
 from os import environ
 # pass through environment
-BASE_FOLDER=environ['BASE_FOLDER']
-GENOTYPE_FOLDER='/%s/GENOTYPES' % BASE_FOLDER
-DATA_FOLDER='/%s/DATA' % BASE_FOLDER
+BASE_FOLDER=environ.get('BASE_FOLDER','./')
+GENOTYPE_FOLDER=environ.get('GENOTYPE_FOLDER','/%s/GENOTYPES' % BASE_FOLDER)
+DATA_FOLDER=environ.get('DATA_FOLDER','/%s/DATA' % BASE_FOLDER)
