03f67db90eabf04e5bec16495703d887a9ae1d1e
Uemit Seren <uemit.seren@gmail.com>
2016-04-13 22:28:32 +0200

    Bugfix: Added support for multiple platforms

--- thehonestgenepipeline/ancestry.py
+++ thehonestgenepipeline/ancestry.py
@@ -4,6 +4,7 @@ from thehonestgenepipeline.celery import celery
 from ancestor.core import ancestry as an  
 from os import path
 from . import GENOTYPE_FOLDER,DATA_FOLDER
+from . import get_platform_from_genotype
 from .progress_logger import CeleryProgressLogHandler 
 import h5py
 
@@ -18,17 +19,16 @@ def setup_task_logger(**kwargs):
     logger.addHandler(progress_handler)
 
 @celery.task(serialiazer='json')
-def analysis(id,weights_file,pcs_file):
+def analysis(id,region):
     try:
         log_extra={'progress':5,'id':id}
         logger.info('Starting Ancestry',extra={'progress':0,'id':id})
         genotype_file= '%s/IMPUTED/%s.hdf5' % (GENOTYPE_FOLDER,id)
         if not path.exists(genotype_file):
             raise Exception('Genotype file %s not found' % genotype_file)
-        # Need to pass in 
-        version = '23andme_v1'
-        weights_file = '%s/%s' % (DATA_FOLDER,weights_file)
-        pcs_file = '%s/%s' % (DATA_FOLDER,pcs_file)
+        platform = get_platform_from_genotype(genotype_file)
+        weights_file = '%s/AN_DATA/%s_weights.hdf5' % (DATA_FOLDER,region)
+        pcs_file = '%s/AN_DATA/hapmap_%s_%s_pcs.hdf5' % (DATA_FOLDER,platform,region)
         ancestry_dict = an.ancestry_analysis(genotype_file,weights_file,pcs_file,log_extra=log_extra)
         result = {'pc1':float(ancestry_dict['pc1']),'pc2':float(ancestry_dict['pc2']),'is_in_population':bool(ancestry_dict['is_in_population']),
                  'pop_mean':ancestry_dict['pop_mean'].tolist(),'pop_std':ancestry_dict['pop_std'].tolist(),'ind_lim':ancestry_dict['ind_lim'].tolist(),'population':ancestry_dict['population']}
