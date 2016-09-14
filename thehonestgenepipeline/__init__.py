661d1aa16579ea99a9d7c7106844309022c4c487
Uemit Seren @ Laptop <uemit.seren@gmail.com>
2016-09-14 22:57:19 +0200

    Save results and pylint fixes

--- thehonestgenepipeline/__init__.py
+++ thehonestgenepipeline/__init__.py
@@ -1,20 +1,39 @@
+"""
+Common utility functions
+"""
+
 from os import environ
+import json
 import h5py
 
 # pass through environment
-BASE_FOLDER=environ.get('BASE_FOLDER','./')
-GENOTYPE_FOLDER=environ.get('GENOTYPE_FOLDER','/%s/GENOTYPES' % BASE_FOLDER)
-DATA_FOLDER=environ.get('DATA_FOLDER','/%s/DATA' % BASE_FOLDER)
+BASE_FOLDER = environ.get('BASE_FOLDER', './')
+GENOTYPE_FOLDER = environ.get('GENOTYPE_FOLDER', '/%s/GENOTYPES' % BASE_FOLDER)
+DATA_FOLDER = environ.get('DATA_FOLDER', '/%s/DATA' % BASE_FOLDER)
 
 def get_platform_from_genotype(filename):
-    f = h5py.File(filename,'r')
+    """
+    Retrieve platform, version and source from imput file
+    """
+    f = h5py.File(filename, 'r')
     platform = None
     try:
         source = f.attrs['source']
         version = f.attrs['version']
         platform = source
         if version is not None and version != '':
-            platform +='_%s' % version
+            platform += '_%s' % version
     finally:
         f.close()
     return platform
+
+def save_analysis_data(filename, data, key):
+    """
+    Save the results from the analysis in the output file
+    """
+    f = h5py.File(filename, 'r+')
+    try:
+        f.attrs[key] = json.dumps(data)
+        f.flush()
+    finally:
+        f.close()
--- thehonestgenepipeline/imputation.py
+++ thehonestgenepipeline/imputation.py
@@ -1,69 +1,89 @@
+"""
+Pipeline for imputation
+"""
+from os import path
 from celery.utils.log import get_task_logger
 from celery.signals import after_setup_task_logger
 from thehonestgenepipeline.celery import celery
-from imputor.core import impute as imp  
-from os import path
-import h5py
-from . import GENOTYPE_FOLDER,DATA_FOLDER
-from . import get_platform_from_genotype
+from imputor.core import impute as imp
+from . import GENOTYPE_FOLDER, DATA_FOLDER
+from . import get_platform_from_genotype, save_analysis_data
 from .progress_logger import CeleryProgressLogHandler
 
-import logging
 
-logger = get_task_logger(imp.__name__)
+
+LOGGER = get_task_logger(imp.__name__)
 
 
 @after_setup_task_logger.connect
 def setup_task_logger(**kwargs):
-    progress_handler = CeleryProgressLogHandler(celery,'imputation')
-    logger.addHandler(progress_handler)
+    """
+    Log messages back to broker
+    """
+    progress_handler = CeleryProgressLogHandler(celery, 'imputation')
+    LOGGER.addHandler(progress_handler)
 
 @celery.task(serialiazer='json')
-def convert(id,log_extra={'progress':0,'max_progress':100}):
+def convert(genotype_id, log_extra=None):
+    """
+    Convert nucleotides to binary encoding
+    """
+    if log_extra is None:
+        log_extra = {'progress':0, 'max_progress':100}
     try:
-        log_extra['id'] = id
-        filename = '%s.hdf5' % id
-        logger.info('Starting Conversion',extra=log_extra)
-        genotype_file= '%s/ORIGINAL/%s' % (GENOTYPE_FOLDER,filename)
-        output_file = '%s/CONVERTED/%s' % (GENOTYPE_FOLDER,filename)
+        log_extra['id'] = genotype_id
+        filename = '%s.hdf5' % genotype_id
+        LOGGER.info('Starting Conversion', extra=log_extra)
+        genotype_file = '%s/ORIGINAL/%s' % (GENOTYPE_FOLDER, filename)
+        output_file = '%s/CONVERTED/%s' % (GENOTYPE_FOLDER, filename)
         if not path.exists(genotype_file):
             raise Exception('Genotype file %s not found' % genotype_file)
-        # Need to pass in 
+        # Need to pass in
         platform = get_platform_from_genotype(genotype_file)
-        nt_map_file = '%s/NT_DATA/%s_nt_map.pickled' % (DATA_FOLDER,platform)
-        result = imp.convert_genotype_nt_key_encoding(genotype_file,output_file,nt_map_file,log_extra=log_extra)
-        logger.info('Finished Conversion',extra={'progress':log_extra.get('max_progress',100),'id':id})
+        nt_map_file = '%s/NT_DATA/%s_nt_map.pickled' % (DATA_FOLDER, platform)
+        result = imp.convert_genotype_nt_key_encoding(genotype_file, output_file, nt_map_file, log_extra=log_extra)
+        save_analysis_data(output_file, result, 'convert')
+        LOGGER.info('Finished Conversion', extra={'progress':log_extra.get('max_progress', 100), 'id':genotype_id})
     except Exception as err:
-        logger.error('Error during conversion',extra=log_extra)
-        raise err
-    return result
-    
-@celery.task(serialiazer='json')
-def impute(id,log_extra={'progress':0,'max_progress':100}):
-    try:
-        log_extra['id'] = id
-        filename = '%s.hdf5' % id
-        logger.info('Starting Imputation',extra=log_extra)
-        genotype_file= '%s/CONVERTED/%s' % (GENOTYPE_FOLDER,filename)
-        output_file = '%s/IMPUTED/%s' % (GENOTYPE_FOLDER,filename)
-        if not path.exists(genotype_file):
-            raise Exception('Genotype file %s not found' % genotype_file)
-        # Need to pass in 
-        platform = get_platform_from_genotype(genotype_file)
-        ld_folder = '%s/LD_DATA/%s' % (DATA_FOLDER,platform)
-        result = imp.impute(genotype_file,ld_folder,output_file,log_extra=log_extra)
-        logger.info('Finished Imputation',extra={'progress':log_extra.get('max_progress',100),'id':id})
-    except Exception as err:
-        logger.error('Error during imputation',extra=log_extra)
+        LOGGER.error('Error during conversion', extra=log_extra)
         raise err
     return result
 
+@celery.task(serialiazer='json')
+def impute(genotype_id, log_extra=None):
+    """
+    Impute missing SNPs
+    """
+    if log_extra is None:
+        log_extra = {'progress':0, 'max_progress':100}
+    try:
+        log_extra['id'] = genotype_id
+        filename = '%s.hdf5' % genotype_id
+        LOGGER.info('Starting Imputation', extra=log_extra)
+        genotype_file = '%s/CONVERTED/%s' % (GENOTYPE_FOLDER, filename)
+        output_file = '%s/IMPUTED/%s' % (GENOTYPE_FOLDER, filename)
+        if not path.exists(genotype_file):
+            raise Exception('Genotype file %s not found' % genotype_file)
+        # Need to pass in
+        platform = get_platform_from_genotype(genotype_file)
+        ld_folder = '%s/LD_DATA/%s' % (DATA_FOLDER, platform)
+        result = imp.impute(genotype_file, ld_folder, output_file, log_extra=log_extra)
+        save_analysis_data(output_file, result, 'imputation')
+        LOGGER.info('Finished Imputation', extra={'progress':log_extra.get('max_progress', 100), 'id':genotype_id})
+    except Exception as err:
+        LOGGER.error('Error during imputation', extra=log_extra)
+        raise err
+    return result
+
 
 @celery.task(serialiazer='json')
-def imputation(id):
+def imputation(genotype_id):
+    """
+    Entire imputation pipeline consisting of convert and impute
+    """
     result = {}
-    logger.info('Starting Imputation Pipeline',extra={'progress':0,'id':id})
-    result['convert'] = convert(id,{'progress':5,'max_progress':20})
-    result['imputation'] = impute(id,{'progress':20,'max_progress':95})
-    logger.info('Finished Imputation Pipeline',extra={'progress':100,'id':id,'state':'FINISHED'})
+    LOGGER.info('Starting Imputation Pipeline', extra={'progress':0, 'id':genotype_id})
+    result['convert'] = convert(genotype_id, {'progress':5, 'max_progress':20})
+    result['imputation'] = impute(genotype_id, {'progress':20, 'max_progress':95})
+    LOGGER.info('Finished Imputation Pipeline', extra={'progress':100, 'id':genotype_id, 'state':'FINISHED'})
     return result
